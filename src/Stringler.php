28945fcb78c52fc1a6c0b8cf883266c6508f8e70
ArpyAge <ricardo_hoek@hotmail.com>
2016-01-05 22:42:08 +0100

    Update Stringler.php

--- src/Stringler.php
+++ src/Stringler.php
@@ -10,27 +10,27 @@ class Stringler
 {
 	private $_string = ""; // saved string from construct or function
 	
-    /**
-     * __construct
-     *
-     * __construct function to save a string to check for multiple things
-     *
-     * @access public
-     * @param  string $string the saved string
-     */
+	/**
+	 * __construct
+	 *
+	 * __construct function to save a string to check for multiple things
+	 *
+	 * @access public
+	 * @param  string $string the saved string
+	 */
 	public function __construct($string = ""){
 		$this->_string = $string;
 	}
 	
 	/**
-     * Fill String
-     *
-     * Fill string function to see whether there is a new string and save it
-     *
-     * @access private
-     * @param  string newly input string
+	 * Fill String
+	 *
+	 * Fill string function to see whether there is a new string and save it
+	 *
+	 * @access private
+	 * @param  string newly input string
 	 * @return string new saved string
-     */
+	 */
 	private function fillString($string) {
 		if($string != '') {
 			$this->_string = $string;
@@ -39,60 +39,60 @@ class Stringler
 	}
 	
 	/**
-     * Starts With
-     *
-     * Starts with function to check whether a string starts with another string
-     *
-     * @access public
-     * @param  string $startsWith string to see whether it starts with this string
+	 * Starts With
+	 *
+	 * Starts with function to check whether a string starts with another string
+	 *
+	 * @access public
+	 * @param  string $startsWith string to see whether it starts with this string
 	 * @param  string $string to check with this, or the saved string
 	 * @return boolean if it starts with the string or not
-     */
+	 */
 	public function startsWith($startsWith, $string = "") 
 	{
 		return substr(self::fillString($string), 0, strlen($startsWith)) == $startsWith;
 	}
 	
 	/**
-     * Ends With
-     *
-     * Ends with function to check whether a string ends with another string
-     *
-     * @access public
-     * @param  string $endsWith string to see whether it ends with this string
+	 * Ends With
+	 *
+	 * Ends with function to check whether a string ends with another string
+	 *
+	 * @access public
+	 * @param  string $endsWith string to see whether it ends with this string
 	 * @param  string $string to check with this, or the saved string
 	 * @return boolean if it ends with the string or not
-     */
+	 */
 	public function endsWith($endsWith, $string = "") 
 	{
 		return substr(self::fillString($string), 0 - strlen($endsWith)) == $endsWith;
 	}
 	
 	/**
-     * Contains
-     *
-     * Contains function to check whether a string contains another string
-     *
-     * @access public
-     * @param  string $endsWith string to see whether it contains this string
+	 * Contains
+	 *
+	 * Contains function to check whether a string contains another string
+	 *
+	 * @access public
+	 * @param  string $endsWith string to see whether it contains this string
 	 * @param  string $string to check with this, or the saved string
 	 * @return boolean if it contains the string or not
-     */
+	 */
 	public function contains($contains, $string = "") 
 	{
 		return strpos(self::fillString($string), $contains) !== false;
 	}
 	
 	/**
-     * Regex
-     *
-     * Regex function to check whether a string complies to a certain regular expression
-     *
-     * @access public
-     * @param  string $pattern pattern for the regular expression
+	 * Regex
+	 *
+	 * Regex function to check whether a string complies to a certain regular expression
+	 *
+	 * @access public
+	 * @param  string $pattern pattern for the regular expression
 	 * @param  string $string to check with this, or the saved string
 	 * @return boolean if it complies to the regular expression
-     */
+	 */
 	public function regex($pattern, $string = "") 
 	{
 		return preg_match($pattern, self::fillString($string));
